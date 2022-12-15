f6e23ef61865d448cd81b1701ab1707dabef59fb
karthikeyananataraj03 <120604313+karthikeyananataraj03@users.noreply.github.com>
2022-12-15 00:11:03 -0500

    Update module1.py

--- module1.py
+++ module1.py
@@ -18,7 +18,7 @@ def predict(hospitalized,hospitalizedCurrently,hospitalizedIncrease,negativeIncr
     
 
     prediction = model.predict(pd.DataFrame([[hospitalized,hospitalizedCurrently,hospitalizedIncrease,negativeIncrease,positiveIncrease,totalTestResultsIncrease,recovered]],
-                                           columns=['hospitalized','hospitalizedCurrently','hospitalizedIncrease','positiveIncrease','totalTestResultsIncrease','recovered']))
+                                           columns=['hospitalized','hospitalizedCurrently','hospitalizedIncrease','negativeIncrease','positiveIncrease','totalTestResultsIncrease','recovered']))
     return prediction
 
 
@@ -26,14 +26,14 @@ def predict(hospitalized,hospitalizedCurrently,hospitalizedIncrease,negativeIncr
 st.title('Covid death count predictor')
 
 st.header('Enter the Details:')
-hospitalized = st.number_input('hospitalized:', min_value=0.1, max_value=100000000.0, value=1.0)
 
-hospitalizedCurrently = st.number_input('hospitalizedCurrently:', min_value=0.1, max_value=100000000.0, value=1.0)
-
-hospitalizedIncrease = st.number_input('hospitalizedIncrease:', min_value=0.1, max_value=100000000.0, value=1.0)
-positiveIncrease = st.number_input('positiveIncrease:', min_value=0.1, max_value=100000000.0, value=1.0)
-totalTestResultsIncrease = st.number_input('totalTestResultsIncrease:', min_value=0.1, max_value=100000000.0, value=1.0)
-recovered = st.number_input('recovered:', min_value=0.1, max_value=100000000.0, value=1.0)
+hospitalized = st.number_input('hospitalized:', min_value=10, max_value=100000000.0, value=1.0)
+hospitalizedCurrently = st.number_input('hospitalizedCurrently:', min_value=10, max_value=100000000.0, value=1.0)
+hospitalizedIncrease = st.number_input('hospitalizedIncrease:', min_value=10, max_value=100000000.0, value=1.0)
+negativeIncrease = st.number_input('negativeIncrease:', min_value=10, max_value=100000000.0, value=1.0)
+positiveIncrease = st.number_input('positiveIncrease:',min_value=10, max_value=100000000.0, value=1.0)
+totalTestResultsIncrease = st.number_input('totalTestResultsIncrease:',min_value=10, max_value=100000000.0, value=1.0)
+recovered = st.number_input('recovered:', min_value=10, max_value=100000000.0, value=1.0)
 
 
 if st.button('Predict Death'):
