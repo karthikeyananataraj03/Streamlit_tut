a0fa12cbabb0c5393f346cf9c76ac3f47a6b8df9
karthikeyananataraj03 <120604313+karthikeyananataraj03@users.noreply.github.com>
2022-12-28 13:40:14 -0500

    Update module1.py

--- module1.py
+++ module1.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 df= pd.read_csv("Stremlit_data.csv")
 df = df.sort_values('1')
 
-st.line_chart(data=df, *, x=date, y=death, width=0, height=0, use_container_width=True)
+st.line_chart(data=df, x=date, y=death, width=0, height=0, use_container_width=True)
 
 
 #Loading up the Regression model we created
